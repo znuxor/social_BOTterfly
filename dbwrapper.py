d808545cfadb85a6eda0aef74891d09cf94421df
znuxor <znuxor@gmail.com>
2017-12-28 20:58:19 -0500

    current work on modules

---
+++ dbwrapper.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sqlite3
+import logging
+
+schema_filepath = './dbschema.sql'
+
+class DBWrapper():
+    ''' Wrapper class around a database to interface the necessary operations.'''
+    def __init__(self, database_file_path, zone_id_func):
+        self.database_file_path = database_file_path
+        self.zone_id_func = zone_id_func
+
+        if not os.path.exists(database_file_path):
+            print('Database does not exist, creating...')
+            self.create_database(database_file_path)
+
+        self.db_connection = sqlite3.connect(database_file_path)
+
+    def create_database(self, database_file_path):
+        ''' Creates the database file and sets the schema.'''
+        self.db_connection = sqlite3.connect(database_file_path)
+        with open(schema_filepath) as file_handle:
+            schema = file_handle.read()
+            self.db_connection.executescript(schema)
+        self.db_connection.close()
+
+    def get_users_in_zone(self, zone_id):
+        ''' Gets a list of all users in the same timezone and their data.'''
+        command = """
+        select * WHERE zone_id=? order by last_suggestion
+        AND
+        banned=false
+        AND
+        unsubscribed=false
+        """
+        self.db_connection.execute(command, (zone_id,))
+        user_data = [i for i in self.db_connection.fetchmany()]
+        print(user_data)
+        return user_data
+
+    def fetch_user(self, username):
+        ''' Fetches a user's data from the database.'''
+        command = """
+        select * where nickname=?
+        """
+        self.db_connection.execute(command, (username, ))
+        user_data = self.db_connection.fetch()
+        print(user_data)
+        return user_data
+
+    def add_user(self, username, latitude, longitude, min_distance_km, max_distance_km, banned=False, favourite_things='', blacklist_things='', suggestion_freq='weekly', communities='socialskills'):
+        ''' Adds a user to the database.'''
+        zone_id = self.zone_id_func(latitude, longitude)
+        command = """
+        INSERT into user_data (nickname, latitude, longitude, zone_id, min_distance_km, max_distance_km, unsubscribed, banned, favourite_things, blacklist_things, suggestion_freq, communities, last_suggestion, already_suggested)
+        values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """
+        self.db_connection.execute(command, (username, latitude longitude, zone_id, min_distance_km, max_distance_km, False, banned, favourite_things, blacklist_things, suggestion_freq, communities, datetime.datetime.utcnow().strftime('%Y-%m-%d'), ''))
+
+    def unsubscribe_user(self, username):
+        ''' Marks a user as unsubscribed'''
+        command = """
+        UPDATE user_data
+        SET unsubscribed=True
+        WHERE nickname=?
+        """
+        self.db_connection.execute(command, (username,))
+
+    def ban_user(self, username):
+        ''' Marks a user as unsubscribed'''
+        command = """
+        UPDATE user_data
+        SET banned=True
+        WHERE nickname=?
+        """
+        self.db_connection.execute(command, (username,))
+
+    def set_position(self, username, latitude, longitude):
+        ''' Changes a user's position'''
+        zone_id = self.zone_id_func(latitude, longitude)
+        command = """
+        UPDATE user_data
+        SET latitude=?, longitude=?, zone_id=?
+        WHERE nickname=?
+        """
+        self.db_connection.execute(command, (latitude, longitude, zone_id, username))
+
+    def set_max_distance(self, username, max_distance_km):
+        ''' Changes a user's max distance'''
+        command = """
+        UPDATE user_data
+        SET max_distance_km=?
+        WHERE nickname=?
+        """
+        self.db_connection.execute(command, (max_distance_km, username))
+
+    def set_min_distance(self, username, min_distance_km):
+        ''' Changes a user's max distance'''
+        command = """
+        UPDATE user_data
+        SET min_distance_km=?
+        WHERE nickname=?
+        """
+        self.db_connection.execute(command, (min_distance_km, username))
+
+    def set_favourite_things(self, username, favourite_things):
+        ''' Changes a user's favourite things'''
+        command = """
+        UPDATE user_data
+        SET favourite_things=?
+        WHERE nickname=?
+        """
+        self.db_connection.execute(command, (favourite_things, username))
+
+    def set_blacklist_things(self, username, blacklist_things):
+        ''' Changes a user's favourite things'''
+        command = """
+        UPDATE user_data
+        SET blacklist_things=?
+        WHERE nickname=?
+        """
+        self.db_connection.execute(command, (blacklist_things, username))
+
+    def set_suggestion_freq(self, username, suggestion_freq):
+        ''' Changes a user's suggestion frequency'''
+        command = """
+        UPDATE user_data
+        SET suggestion_freq=?
+        WHERE nickname=?
+        """
+        self.db_connection.execute(command, (suggestion_freq, username))
+
+    def set_communities(self, username, communities):
+        ''' Changes a user's community list'''
+        command = """
+        UPDATE user_data
+        SET communities=?
+        WHERE nickname=?
+        """
+        self.db_connection.execute(command, (communities, username))
+
+    def update_with_suggestion(self, username, suggestion):
+        command = """
+        UPDATE user_data
+        SET 
+        WHERE nickname=?
+        """
+        self.db_connection.execute(command, (username))
+
+    def cleanup_inactive_users(self, username):
+        raise NotImplemented
+
+    def __delete__(self, instance):
+        self.db_connection.commit()
+        self.db_connection.close()
---
+++ redditwrapper.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import praw
+
+class RedditWrapper():
+    ''' Wrapper class around reddit to interact with the bot account.'''
+
+    def __init__(self, client_id, client_secret, bot_password):
+        bot_username = 'social_BOTterfly'
+        self.reddit = praw.Reddit(client_id=client_id,
+                                  client_secret=client_secret,
+                                  username=bot_username,
+                                  password=bot_password,
+                                  user_agent='social_BOTterfly by /u/znuxor')
+        test_username = self.reddit.user.me()
+        assert test_username == bot_username, 'Could not authenticate to the bot account!'
+
+    def has_unread_messages(self):
+        '''Returns True if there is messages to process'''
+        return len(list(self.reddit.inbox.unread(limit=1))) == 1
+
+    def serve_oldest_message(self):
+        '''Returns the following information about the oldest inbox message:
+           author_username as string
+           message_subject as string
+           message_body as string
+           message_id as string, used to mark a message as read after processing
+        '''
+        unread_message = [i for i in self.reddit.inbox.unread(limit=None)][-1]
+        author_username = unread_message.author.name
+        message_subject = unread_message.subject
+        message_body = unread_message.body
+        message_id = unread_message.id
+        return author_username, message_subject, message_body, message_id
+
+    def mark_as_read(self, message_id):
+        ''' Mark a message as read, takes the reddit PRAW message id as argument'''
+        message_obj = self.reddit.inbox.message(message_id)
+        message_obj.mark_read()
+
+    def send_message(user_name, subject, body):
+        ''' Sends a message to a redditor'''
+        self.reddit.redditor(user_name).message(subject, body)
