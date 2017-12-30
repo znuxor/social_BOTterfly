1390fb9db2ec069c69623afe3ed20456d2c3b4cb
znuxor <znuxor@gmail.com>
2017-12-30 01:51:31 -0500

    some more work into the big function of messageprocessor

--- socialbotterfly/messageprocessor.py
+++ socialbotterfly/messageprocessor.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from .informatiionmanager import InformationManager
+
+help_message_filename = 'help_message.txt'
+absolute_help_file_path = os.path.join(
+        os.path.dirnam(__file__), help_message_filename)
+
 class MessageProcessor():
     '''Classes that processes messages and does the actions needed'''
     def __init__(self, redditwrapper, dbwrapper):
@@ -27,6 +33,26 @@ class MessageProcessor():
             # mark the message as valid
             self.redditwrapper.mark_as_read(message_id)
 
-    def send_help_message(author, failure_information):
-        with 
-        help_message = 
+    def send_help_message(self, author, failure_information):
+        with  open(absolute_help_file_path, 'r') as f:
+            help_message = f.read()
+        self.redditwrapper.send_message(author, 'social_BOTterfly Help message', help_message.format(author, failure_information))
+
+    def process_subscribe_message(self, author, body):
+        all_needed_info_available = False
+
+        if all_needed_info_available:
+            self.dbwrapper.add_user(author, latitude, longitude, zone_id, max_distance_km,favourite_things=favourite_things, blacklist_things=blacklist_things, suggestion_freq=suggestion_freq, communities=communities)
+            self.redditwrapper.send_message(author, 'subscription successful', 'Your subscription was successful, be prepared to receive suggestion soon!')
+        return True, None
+
+    def process_unsubscribe_message(self, author):
+        self.dbwrapper.unsubscribe_user(author)
+        self.redditwrapper.send_message(author, 'unsubscription successful', 'Your unsubscription was successful')
+        return True, None
+
+    def process_comment_message(self, author, body):
+        the_logger = InformationManager().get_project_logger()
+        the_logger.info("Comment by /u/"+author + ": " + body)
+        self.redditwrapper.send(author, 'Thank you for your comment!', 'Thank you for your comment for social_BOTterfly!')
+        return True, None
